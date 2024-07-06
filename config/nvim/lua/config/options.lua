9122159ef37b41114b2f64413784e3c67bcdf419
Elfry <elfry3@gmail.com>
2024-07-07 01:24:56 +0700

    Improve aesthetics

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -15,11 +15,11 @@ if vim.g.neovide then
   -- Found on or inspired by something found on the following urls:
   -- - https://github.com/neovide/neovide/issues/1553 and https://www.reddit.com/r/neovim/comments/13ia46q/make_window_title_nvim_pathtofiletxt/
   -- - https://www.reddit.com/r/neovim/comments/13ia46q/make_window_title_nvim_pathtofiletxt/
-  -- - https://stackoverflow.com/a/75897088
+  -- - https://stackoverflow.com/a/69669289
   -- - https://stackoverflow.com/questions/1405583/concatenation-of-strings-in-lua
   vim.opt.title = true
   vim.opt.titlelen = 0 -- do not shorten title
-  vim.opt.titlestring = vim.loop.cwd() .. " - Neovide"
+  vim.opt.titlestring = vim.api.nvim_buf_get_name(0) .. " - Neovide"
 end
 
 -- Mark column 80.
