9ae2620f5783241247893a5504ca80ee056566f8
Elfry <elfry3@gmail.com>
2025-10-15 17:26:10 +0700

    Install Neovim via Scoop

--- RefreshEnv.ps1
+++
@@ -1,3 +0,0 @@
-# Source: https://stackoverflow.com/a/31845512.
-
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
--- Uninstall.ps1
+++ Uninstall.ps1
@@ -1,10 +1,7 @@
 # Uninstall the required Scoop packages.
-scoop uninstall make python nodejs cargo-binstall ripgrep lazygit CascadiaCode-NF neovide
+scoop uninstall make python nodejs cargo-binstall ripgrep lazygit CascadiaCode-NF neovim neovide
 
 # Uninstall LunarVim.
 Invoke-WebRequest https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/uninstall.ps1 -UseBasicParsing | Invoke-Expression
 
-# Refresh the environment variables.
-.\RefreshEnv.ps1
-
-echo "kami uninstalled. The script couldn't uninstall Neovim and CascadiaCode-NF; they have to be uninstalled manually."
+echo "kami uninstalled."
