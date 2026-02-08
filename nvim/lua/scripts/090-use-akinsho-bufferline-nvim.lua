0c2a6041d76b0106569bca94c600fc67499bb861
Elfry <elfry3@gmail.com>
2026-02-08 14:45:43 +0700

    Enable bufferline offset

--- nvim/lua/scripts/090-use-akinsho-bufferline-nvim.lua
+++ nvim/lua/scripts/090-use-akinsho-bufferline-nvim.lua
@@ -1,8 +1,22 @@
 MiniDeps.add({
-  source = 'akinsho/bufferline.nvim',
-  -- Supply dependencies near target plugin
-  depends = { 'nvim-tree/nvim-web-devicons' },
+	source = 'akinsho/bufferline.nvim',
+	-- Supply dependencies near target plugin
+	depends = { 'nvim-tree/nvim-web-devicons' },
 })
 
 vim.opt.termguicolors = true
-require("bufferline").setup{}
+require("bufferline").setup{
+	options = {
+		offsets = {
+			{
+				-- filetype = "NvimTree",
+				filetype = "neo-tree",
+				-- text = "File Explorer" | function ,
+				text = "",
+				-- text_align = "left" | "center" | "right"
+				-- text_align = "left",
+				separator = true,
+			}
+		}
+	}
+}
